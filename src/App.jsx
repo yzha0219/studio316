b3c1b9d7636f99e54021172f713e93f6f825c969
Zhang Yi <zhang.yi0110@163.com>
2021-12-29 23:04:25 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import {useState, useEffect} from "react";
+import {Navigation} from "./components/navigation";
+import {Header} from "./components/header";
+import {Features} from "./components/features";
+import {About} from "./components/about";
+import {Services} from "./components/services";
+import {Gallery} from "./components/gallery";
+import {Testimonials} from "./components/testimonials";
+import {Team} from "./components/Team";
+import {Contact} from "./components/contact";
+import JsonData from "./data/data.json";
+import SmoothScroll from "smooth-scroll";
+import "./App.css";
+import Lightbox from 'react-lightbox-component';
+import './lightbox.css'
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000,
+    speedAsDuration: true,
+});
+
+const App = () => {
+    const [landingPageData, setLandingPageData] = useState({});
+    useEffect(() => {
+        setLandingPageData(JsonData);
+    }, []);
+
+    return (
+        <div>
+            <Navigation/>
+            <Header data={landingPageData.Header}/>
+            <Features data={landingPageData.Features}/>
+            <About data={landingPageData.About}/>
+            <Services data={landingPageData.Services}/>
+            <Gallery data={landingPageData.Gallery}/>
+            <Testimonials data={landingPageData.Testimonials}/>
+            <Team data={landingPageData.Team}/>
+            <Contact data={landingPageData.Contact}/>
+            <Lightbox images={ [
+                {
+                    src: 'https://upload.wikimedia.org/wikipedia/commons/8/89/Ropy_pahoehoe.jpg',
+                    title: 'Ropy pahoehoe',
+                    description: 'By Tari Noelani Mattox. Licensed under Public Domain via Commons'
+                },
+                {
+                    src: 'https://upload.wikimedia.org/wikipedia/commons/7/73/Pyroclastic_flows_at_Mayon_Volcano.jpg',
+                    title: 'Pyroclastic flows at Mayon Volcano',
+                    description: 'By C.G. Newhall. Licensed under Public Domain via Commons'
+                },
+                {
+                    src: 'https://upload.wikimedia.org/wikipedia/commons/f/f3/Okataina.jpg',
+                    title: 'Okataina',
+                    description: 'By Richard Waitt. Licensed under Public Domain via Commons'
+                }
+            ] }/>
+        </div>
+    );
+};
+
+export default App;
